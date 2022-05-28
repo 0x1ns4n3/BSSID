c89e833a366d72a2a55b41d0d09127c7b3bfedde
Dikshant Singh <95041015+0x1ns4n3@users.noreply.github.com>
2022-05-28 18:57:02 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,4 +4,4 @@ import pandas as pd
 wd = pd.read_csv ('./file.csv', delimiter = ',')  # read warflight csv file into Pandas dataframe
 
 print("Total WiFi AP entries: " + str(len(wd)))
-wd.sample(10)  # 10 random networks
+wd.sample(4000)  # 4000 random networks
